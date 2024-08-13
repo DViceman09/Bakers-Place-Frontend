98fe2fb3f50c02863640e4bd29f8801f88d9aeaa
DViceman09 <dvfrost09@gmail.com>
2024-08-13 12:00:06 +0530

    Added Redux for Cart and Menu

--- src/State/Authentication/store.js
+++ src/State/Authentication/store.js
@@ -2,12 +2,14 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import {thunk} from "redux-thunk";
 import Reducer from "./Reducer";
 import restaurantReducer from "../Restaurant/Restaurant_Reducer";
+import menuItemReducer from "../Menu/Menu_Reducer";
 
 
 const rootReducer=combineReducers({
 
     auth:Reducer,
-    restaurant : restaurantReducer
+    restaurant : restaurantReducer,
+    menu:menuItemReducer
 })
 
 export const store = legacy_createStore(rootReducer,applyMiddleware(thunk))
