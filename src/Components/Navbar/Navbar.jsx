db978d1f9c08ac34a1c322599d10f2608da36ed4
DViceman09 <dvfrost09@gmail.com>
2024-09-17 17:57:23 +0530

    Made a few small changes and changed the BASE url to the new backend server

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -20,12 +20,12 @@ export const Navbar = () => {
         {navigate("/admin/restaurant")}
 }
     return (
-        <Box className='px-5 sticky top-0 z-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex
+        <Box className='px-5 sticky top-0 z-50 py-[.8rem] bg-[#e91e62] lg:px-20 flex
         justify-between'>
             <div className='flex items-center space-x-4'>
                 <div className='lg:mr-10 cursor-pointer flex items-center space-x-4'>
                     <li onClick={()=> navigate("/")} className='logo font-semibold text-gray-300 text-2xl'>
-                        Anne's Bakery
+                        Baker's Place
                     </li>
                 </div>
             </div>
